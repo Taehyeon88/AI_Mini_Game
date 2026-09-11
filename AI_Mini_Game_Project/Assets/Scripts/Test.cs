03ff84c033637b2045f88bde890162fe6ee704ec
Taehyeon88 <Taehyeon5376@naver.com>
2026-09-11 23:54:18 +0900

    [Pro Feat] Unity mcp 및 Claude Code 셋팅

--- AI_Mini_Game_Project/Assets/Scripts/Test.cs
+++ AI_Mini_Game_Project/Assets/Scripts/Test.cs
@@ -6,12 +6,13 @@ public class Test : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gm = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        gm.transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gm.transform.position = new Vector3(0, 1, 0.2f);
     }
 }
